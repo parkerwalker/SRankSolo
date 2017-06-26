c6a0a4617604e5a1b470a3d060e419a1c7d5d568
Parker Walker <parkerwalker12@gmail.com>
2017-06-26 09:20:57 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var index = require('./modules/routes/index')
+
+app.use(express.static('public'));
+app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use( bodyParser.json() );
+
+
+app.use('/', index);
+var port = process.env.PORT || 3000;
+
+app.listen( port, function(){
+  console.log('server up on:', port);
+})//end listen
