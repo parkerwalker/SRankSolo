a568608371693d824257348d36330bbe048d6e43
Parker Walker <parkerwalker12@gmail.com>
2017-06-27 15:38:20 -0500

    added styling and jumbotron

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,21 +7,34 @@ app.controller('MainController', function(SearchService){
 
   vm.summonerSearch = {};
   vm.recentMatchData = [];
+  vm.championMastery= [];
 
   vm.summonerInput = function(){
+    vm.summonerSearch = {};
+    vm.recentMatchData = [];
+    vm.championMastery = [];
     var summonerName = vm.summonerName;
+    vm.summonerName = '';
     var searchUrl = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + summonerName + '?api_key=RGAPI-a29e06a2-b330-4bdb-adaf-82e3ba71c924';
 
     SearchService.searchSummoner(searchUrl).then(function(data){
       vm.summonerSearch = data;
-      console.log('in controller', data);
+      console.log(vm.summonerSearch);
+
+      var summonerId = vm.summonerSearch.id;
+      SearchService.searchSummonerMastery(summonerId).then(function(data) {
+        for (var i = 0; i < 5; i++) {
+          vm.championMastery.push(data[i])
+        }
+        console.log(vm.championMastery);
+      });//end searchSummonerMastery call
 
       var accountId = vm.summonerSearch.accountId;
       SearchService.searchMatch(accountId).then(function(returnData){
         vm.recentMatchData = returnData.matches;
         for (var i = 0; i < vm.recentMatchData.length; i++) {
-          vm.recentMatchData[i].specificMatchPlayers = [];
-        }
+          vm.recentMatchData[i].specificMatchPlayers = [];//this sets aside an empty array to file with specificMatchCall data
+        }//end for loop
         console.log(vm.recentMatchData);
 
       });//end searchservice.searchmatch call
@@ -37,18 +50,32 @@ vm.specificMatchCall = function(index){
 
       if (vm.specificMatchData.participantIdentities[i].player == null || vm.specificMatchData.participantIdentities[i].player == 0){
         players = {
-          summonerName: 'Unavalable'
+          id: i + 1,
+          summonerName: 'Unavalable',
+          champion: vm.specificMatchData.participants[i].championId,
+          kills: vm.specificMatchData.participants[i].stats.kills,
+          deaths: vm.specificMatchData.participants[i].stats.deaths,
+          assists: vm.specificMatchData.participants[i].stats.assists,
+          win: vm.specificMatchData.participants[i].stats.win,
+          rank: vm.specificMatchData.participants[i].highestAchievedSeasonTier
         }
       }else{
         players = {
-          summonerName: vm.specificMatchData.participantIdentities[i].player.summonerName
+          id: vm.specificMatchData.participantIdentities[i].player.participantId,
+          summonerName: vm.specificMatchData.participantIdentities[i].player.summonerName,
+          champion: vm.specificMatchData.participants[i].championId,
+          kills: vm.specificMatchData.participants[i].stats.kills,
+          deaths: vm.specificMatchData.participants[i].stats.deaths,
+          assists: vm.specificMatchData.participants[i].stats.assists,
+          win: vm.specificMatchData.participants[i].stats.win,
+          rank: vm.specificMatchData.participants[i].highestAchievedSeasonTier
         }
       }//end else
 
       vm.recentMatchData[index].specificMatchPlayers.push(players);
     }//end for loop
 
-    console.log(vm.specificMatchData);
+    console.log(vm.specificMatchData, players);
   });//end searchservice.specificMatch
 };//end specificMatchCall
 
--- public/scripts/services/SearchService.js
+++ public/scripts/services/SearchService.js
@@ -4,11 +4,17 @@ app.service('SearchService', function($http){
   ss = this;
 
   ss.searchSummoner = function(url){
-
     return $http.get(url).then(function(response){
       return response.data;
     });//end searchUrl
   };//end searchGif
+
+  ss.searchSummonerMastery = function(summonerId){
+    ss.masteryUrl = 'https://na1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/' + summonerId + '?api_key=RGAPI-a29e06a2-b330-4bdb-adaf-82e3ba71c924'
+    return $http.get(ss.masteryUrl).then(function(response){
+      return response.data;
+    })
+  };//end searchSummonerMastery
 
   ss.searchMatch = function(accountId){
     ss.matchListUrl = 'https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/' + accountId + '/recent?api_key=RGAPI-a29e06a2-b330-4bdb-adaf-82e3ba71c924'
